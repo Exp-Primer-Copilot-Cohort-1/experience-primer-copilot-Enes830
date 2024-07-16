922865ab2250731fac6deb22805576664adf2f89
Enes Halit <75444934+Enes830@users.noreply.github.com>
2024-07-16 23:27:52 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,6 +1,34 @@
 // Create web server
-const express = require('express');
-const app = express();
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/comment.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "comment.html" );
 });
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+       name:req.body.name,
+       comment:req.body.comment
+   };
+   console.log(response);
+   fs.readFile( __dirname + "/" + "comment.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data.push(response);
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port);
+})
